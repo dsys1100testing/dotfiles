2e98b8b06471ec74a304c431f6476540a2207515
dsys1100testing <danila.sysoev40+1@gmail.com>
2025-01-31 17:00:10 +0400

    obTests

--- openbox.sh
+++ openbox.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 mv ~/.config/openbox ~/.config/openbox-old/
 mkdir -p ~/.config/openbox/
-wget -O ~/.config/openbox/autostart https://raw.githubusercontent.com/dsys1100/dotfiles/refs/heads/main/openbox/openbox/autostart
-wget -O ~/.config/openbox/rc.xml https://raw.githubusercontent.com/dsys1100/dotfiles/refs/heads/main/openbox/openbox/rc.xml
+wget -O ~/.config/openbox/autostart https://raw.githubusercontent.com/dsys1100testing/dotfiles/refs/heads/main/openbox/openbox/autostart
+wget -O ~/.config/openbox/rc.xml https://raw.githubusercontent.com/dsys1100testing/dotfiles/refs/heads/main/openbox/openbox/rc.xml
 mv ~/.config/tint2/ ~/.config/tint2-old/
 mkdir -p ~/.config/tint2/
-wget -O ~/.config/tint2/tint2rc https://raw.githubusercontent.com/dsys1100/dotfiles/refs/heads/main/openbox/tint2/tint2rc
+wget -O ~/.config/tint2/tint2rc https://raw.githubusercontent.com/dsys1100testing/dotfiles/refs/heads/main/openbox/tint2/tint2rc
